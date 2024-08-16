7abe0cdce4faaa9eb0348aa7431c3ff28b8c38e6
Chinmay Anaokar <canaokar3@gatech.edu>
2024-08-16 12:08:28 +0100

    method added for axios

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', async (req,res) => {
+    console.log("There's someone here!!!!");
+    res.json({message: 'You have landed on my empty page, to get weather information, go to /weather'});
+});
+
+app.get('/weather', async (req,res) => {
+    console.log("Weather request received.");
+
+    const lat = 52.9500;
+    const lon = -1.1500;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    
+});
+
 app.listen(port, () => {
     console.log(`Yay! The server is running on http://localhost:${port}`);
 });
